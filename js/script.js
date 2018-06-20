4e32acd8374f414db3aa3fff34bf94ff37d9be82
Louis Chung <40413473+Wahhhhhhhh@users.noreply.github.com>
2018-06-19 23:46:41 -0700

    changes made after 6/19 course content

--- js/script.js
+++ js/script.js
@@ -1,16 +1,132 @@
 /* text text */
 // this is a single line comment 
 
-var title = "News on Monday";
-var desc = "Someone got killed";
+/*
+var first = "Louis";
 
-document.write ("<h1>"+title+"</h1>"+desc);
+function first_name(){
 
-window.alert("alert 1");
-window.alert("alert 2");
-window.alert("alert 3");
+	var second = "Chung";
+	document.write(first+" "+second);
+}
 
-var title = "Welcome to my website";
+first_name();
 
-document.write("<h1>"+title+"</h1>")
+var st = 3;
+var nd = 2;
+var calc = st + 2;
 
+window.alert(calc+nd);
+
+var Louis = 5;
+
+Louis /= 2;
+
+window.alert(Louis);
+
+== /* is equal to */
+/* != /* is not equal to*/
+/* === /*strict is equal to */
+/* !== // strict is not equal to
+/* > // is greater than
+ < // is less than
+>= // greater than or equal
+<= // less than or equal
+
+4 == 4
+
+(4+1) == (3+1)
+
+4 != 4 // false
+4 != 5 // true
+4 > 3 // true
+4 < 3 // false
+*/
+
+/* and operator
+(2==2) && (3==3) // TRUE
+(2==1) && (3==3) // FALSE
+
+or operator
+(2==2) || (3==3) //TRUE
+
+not operator
+!(33 == 33)
+!(33 > 35)
+*/
+
+/*
+var car = ""
+if(car != ""){
+	document.write("it's a string with characters");
+}else{
+		document.write("it's a string with no characters");
+}
+*/
+
+/*
+var car = "Mercedes";
+var car2 = "Ferrar"
+
+if (car == "Mercedes"){
+	window.alert("car variable is a Mercedes, let's check next variable...");
+		if (car2 == "Ferrari"){
+				window.alert("car 2 variable is a Ferrari");
+		}else{
+				window.alert("car 2 variable is not a Ferrari");
+		}
+}else{
+		window.alert("car variable is not Mercedes!")
+}
+*/
+
+/*
+var num1 = 3;
+var num2 = 2;
+
+if ((num1 > 4) || (num2 > 3)) 
+		document.write ("It works!");
+else
+	document.write("The condition is false!");
+*/
+
+
+/*
+var num1 = 2;
+var num2 = 3;
+if (num1 > 5) {
+	document.write("num1 is bigger than one");
+}else if(num2 < 1){
+	document.write("num2 is less than 2");
+}else if(num1 <= 2) {
+	document.write("it works");
+}else{
+	document.write("none of the variables match with the conditions");
+}
+*/
+
+/*
+var name = "Louis";
+switch (name){
+	case "John":
+		document.write("The variable is John");
+		break;
+	case "Mark":
+		document.write("The variable is Mark");
+		break;
+	case "Victor":
+		document.write("The variable is Victor");
+		break;
+	default:
+		document.write("None of the values match the variable");
+}
+*/
+
+/*
+var name = window.prompt("Please type your name below");
+if (name == "") {
+	document.write("Please type something");
+}else{
+	document.write("Welcome "+name);
+}
+*/
